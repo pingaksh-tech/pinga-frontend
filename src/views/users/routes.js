fb891e4cd20fb1a8b3b40e6c0edcb879fdea0dac
Darshan <darshan.micrasol@gmail.com>
2025-06-19 17:35:00 +0530

    change Users to users
    change Users to users

---
+++ src/views/users/routes.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    path: '/users/list',
+    name: 'users-list',
+    component: () => import('@/views/users/Users.vue'),
+    meta: {
+      allowAnonymous: false,
+      pageTitle: 'User List',
+      breadcrumb: [{ title: 'Dashboard', url: '/dashboard' }, { title: 'User' }, { title: 'User List', active: true }]
+    }
+  }
+]
